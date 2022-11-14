a697cc121d3ef8d46f797e9a11671d78a67d3f86
M2kishore <kishoremmofficial@gmail.com>
2022-11-14 22:57:04 +0530

    server - post,delete,get and getid client - update(not in server),delete,show based on route, mongo not yet started

--- server/index.js
+++ server/index.js
@@ -2,23 +2,47 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 const cors=require('cors');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 app.use(cors());
 
-blogs = [
+let bugs = [
    { id: 1, title: 'Clean House', description: 'clean using thodapam', done: false, priority: "High", status: "Pending" },
    { id: 2, title: 'Cannot Reach page', description: '404 error not able to find', done: false, priority: "Low", status: "Todo" }
 ];
-app.get('/blog', function (req, res) {
-   console.log("get all blogs");
-   res.send(blogs);
+app.get('/bug', function (req, res) {
+   console.log("get all bugs");
+   res.send(bugs);
 });
 
-app.get('/blog/:id/', function (req, res) {
-   let blogId = req.params.id;
-   console.log("blog: "+blogId);
-   let blog = blogs.filter(blog => blog.id === parseInt(blogId));
-   res.send(blog[0]);
-})
+app.get('/bug/:id/', function (req, res) {
+   let bugId = req.params.id;
+   console.log("bug: "+bugId);
+   let bug = bugs.filter(bug => bug.id === parseInt(bugId));
+   res.send(bug[0]);
+});
+
+app.post('/bug', function (req, res) {
+   console.log(req.body);
+   bugs.push({
+      id: req.body.id,
+      title: req.body.title,
+      description: req.body.description,
+      priority: req.body.priority,
+      status: req.body.status,
+      done: false
+   })
+   res.send();
+});
+app.delete('/bug/:id', (req, res) => {
+   let bugId = req.params.id;
+   let filteredBugs = bugs.filter((bug=>{
+      return bug.id != bugId;
+   }));
+   bugs = [...filteredBugs]
+   console.log(bugs);
+   res.send();
+ });
 
 var server = app.listen(8080, function () {
    var host = server.address().address
